a9c623e1b89ab12d08d1d31520dc186a56d079e1
Amable Sebastian Quishpe <105821062+SebasDh56@users.noreply.github.com>
2026-06-23 20:23:41 -0500

    LabValida

---
+++ src/main/java/com/clase/indexcontroller/suma/suma.java
@@ -0,0 +1,9 @@
+package com.clase.indexcontroller.suma;
+
+public class suma {
+
+    public int suma(int x, int y) {
+        int resultado = x + y;
+        return resultado;
+    }
+}
---
+++ src/test/java/com/clase/indexcontroller/suma/SumaTest.java
@@ -0,0 +1,37 @@
+package com.clase.indexcontroller.suma;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SumaTest {
+
+    suma suma = new suma();
+
+    @BeforeEach
+    void before() {
+        System.out.println("Before: Iniciando Test Suma");
+    }
+
+    @Test
+    void sumaPositivosTest() {
+        System.out.println("SumaPositivosTest: Iniciando Test Suma");
+        assertEquals(8, suma.suma(3, 5));
+    }
+
+    @Test
+    void sumaNegativosTest() {
+        assertEquals(-8, suma.suma(-3, -5));
+    }
+
+    @Test
+    void sumaCeroTest() {
+        assertEquals(5, suma.suma(0, 5));
+    }
+
+    @AfterEach
+    void after() {
+        System.out.println("After: Finalizando Test Suma");
+    }
+}
